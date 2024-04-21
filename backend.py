7e96b26de3bfe5db34c745f2a3a196bf6ce42627
unsubstantiated-Script <jusfulton@gmail.com>
2024-04-20 18:17:57 -0700

    Chat data processed and plotted

---
+++ backend.py
@@ -0,0 +1,19 @@
+import os
+
+import requests
+
+API_KEY = "70364f554536e98c01778e000b6c53ec"
+
+
+def get_data(place, forecast_days=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    return filtered_data
+
+
+if __name__ == '__main__':
+    print(get_data(place="Tokyo", forecast_days=3))
